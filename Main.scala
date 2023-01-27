fe94dd1a6ecf11ce525bb3c829b78f96c9d0a60c
Pablo <92599790+GloomiePie@users.noreply.github.com>
2023-01-27 00:55:55 -0500

    Avance

--- Main.scala
+++ Main.scala
@@ -7,21 +7,20 @@ import com.cibo.evilplot.plot.aesthetics.DefaultTheme._
 import java.io.File
 
 object Main extends App {
-  val reader = CSVReader.open(new File("C:\\Users\\SALA A\\Desktop\\data_parcial_2_OO.csv"))
+  val reader = CSVReader.open(new File("C:\\Users\\USUARIO\\Downloads\\data_parcial_2_OO.csv"))
   val data = reader.allWithHeaders()
   reader.close()
   //Pregunta 1.1
   val idTournament = data
     .flatMap(elem => elem.get("tourney_id"))
 
-  print("Se jugaron en total: " + idTournament.distinct.size)
+  println("Se jugaron en total: " + idTournament.distinct.size +" Torneos")
 
   //Pregunta 1.2
   val torneos = data.flatMap(x => x.get("tourney_name")).distinct
   val id_torneos = data.flatMap(x => x.get("tourney_id")).distinct
-  val numTorneos = torneos.size
 
-  println(numTorneos + "\n")
+  println("Nombres de los torneos")
   torneos.foreach(x => printf("%s\n", x))
   id_torneos.foreach(x => printf("%s\n", x))
 
@@ -33,9 +32,9 @@ object Main extends App {
   val handNoNulls = hand.map(_.as[String]).filter(x => x != "null")
   val maxHand = handNoNulls.groupBy(x => x).toList.maxBy(_._2.size)._2.size
   val minHand = handNoNulls.groupBy(x => x).toList.minBy(_._2.size)._2.size
-
-  println(maxHand)
-  println(minHand)
+  //Falta gráficos y promedio, cambiar eso
+  println("Máximo de hands" + maxHand)
+  println("Mínimo de hands" +minHand)
 
   //Height
   val datosHeight = data
@@ -48,17 +47,17 @@ object Main extends App {
       case None => 0
     })
 
-  println(datosHeight.max)
-  println(datosHeight.filter(x => x != 0).min)
+  println("Máximo de altura" + datosHeight.max)
+  println("Mínimo de altura" + datosHeight.filter(x => x != 0).min)
   println("El promedio de la altura: " + datosHeight.map(x => x).sum / datosHeight.size)
   println("El promedio de la altura sin nulls: " + datosHeight.map(x => x).sum / datosHeight
     .filter(x => x != 0).size)
 
   val height: Seq[Double] = Seq(datosHeight.max, datosHeight.filter(x => x != 0).min,
     datosHeight.map(x => x).sum / datosHeight.size, datosHeight.map(x => x).sum / datosHeight
-      .filter(x => x != 0).size)
+      .filter(x => x != 0).size, 0)
 
-  val height2 = List("Máximo", "Mínimo", "Promedio sin 0", "Promedio con 0")
+  val height2 = List("Máximo", "Mínimo", "Promedio con 0", "Promedio sin 0")
 
   BarChart(height)
     .title("Datos descriptivos")
@@ -68,14 +67,23 @@ object Main extends App {
     .yLabel("Hands")
     .bottomLegend()
     .render()
-    .write(new File("C:\\Users\\SALA A\\Downloads\\Representación1.png"))
-//sets - best_of
-  println("-------------------------------")
-  println("El partido con mas sets: " + sets.max)
+    .write(new File("C:\\Users\\USUARIO\\Downloads\\Height.png"))
+  //sets - best_of
+  val sets = data
+    .flatMap(row => row.get("match_info"))
+    .map(row => Json.parse(row))
+    .flatMap(jsonData => jsonData \\ "best_of")
+    .map(jsValue => jsValue)
+    .map(jsValue => jsValue.asOpt[Int] match {
+      case Some(i) => i
+      case None => 0
+    })
+
+  println("El partido con más sets: " + sets.max)
   println("El partido con menos sets: " + sets.filter(x => x != 0).min)
   println("El promedio de los sets: " + sets.map(x => x).sum / sets.size)
 
-  val set1: Seq[Double] = Seq(sets.max, sets.filter(x => x != 0).min, sets.map(x => x).sum / sets.size)
+  val set1: Seq[Double] = Seq(sets.max, sets.filter(x => x != 0).min, sets.map(x => x).sum / sets.size, 0)
   val set2 = List("Máximo", "Mínimo", "Promedio")
   BarChart(set1)
     .title("Datos descriptivos")
@@ -85,9 +93,40 @@ object Main extends App {
     .yLabel("Sets")
     .bottomLegend()
     .render()
-    .write(new File("C:\\Users\\SALA A\\Downloads\\Representación2.png"))
+    .write(new File("C:\\Users\\USUARIO\\Downloads\\Sets.png"))
 
-//Pregunta 3
+  //minutes
+  val minutes = data
+    .flatMap(row => row.get("match_info"))
+    .map(row => Json.parse(row))
+    .flatMap(jsonData => jsonData \\ "minutes")
+    .map(jsValue => jsValue).map(jsValue => jsValue.asOpt[Int] match {
+    case Some(i) => i
+    case None => 0
+  })
+
+  println("El máximo de minutos es: " + minutes.max)
+  println("El mínimo de minutos es: " + minutes.filter(x => x != 0).min)
+  println("El promedio de minutos es: " + minutes.map(x => x).sum/ minutes.size)
+  println("El promedio de minutos, sin contra 0, es: " + minutes.map(x => x).sum
+    / minutes.filter(x => x != 0).size)
+
+  val minutes1: Seq[Double] = Seq(minutes.max, minutes.filter(x => x != 0).min,
+    minutes.map(x => x).sum/ minutes.size, minutes.map(x => x).sum
+      / minutes.filter(x => x != 0).size)
+
+  val minutes2 = List("Máximo", "Mínimo", "Promedio con 0", "Promedio sin 0")
+  BarChart(minutes1)
+    .title("Datos descriptivos")
+    .xAxis(minutes2)
+    .yAxis()
+    .frame()
+    .yLabel("Minutes")
+    .bottomLegend()
+    .render()
+    .write(new File("C:\\Users\\USUARIO\\Downloads\\Minutes.png"))
+  
+  //Pregunta 3
   println("-------------------------------")
   println("- ¿Cuál fue el puntaje y numeros de sets que se jugaron en el partido que más minutos duro?")
   val puntaje = data
@@ -105,13 +144,6 @@ object Main extends App {
       case Some(i) => i
       case None => 0
     })
-
-  val sets = data
-    .flatMap(row => row.get("match_info"))
-    .map(row => Json.parse(row))
-    .flatMap(jsonData => jsonData \\ "best_of")
-    .map(jsValue => jsValue)
-    .map(jsValue => jsValue.as[Int])
 
   val setScoreMins = sets.zip(puntaje).zip(minutosJugados)
   println(setScoreMins.map(x => x).sortBy(_._2).reverse.take(1).map(x => x._1))
